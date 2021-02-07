57564c745753849238545051b1db9dec813dfb71
Mahmoud El-Sharkawy <54472692+Sharkawy777@users.noreply.github.com>
2021-02-07 18:13:38 +0200

    Update Essential_Matrix.py

--- Essential_Matrix.py
+++ Essential_Matrix.py
@@ -1,7 +1,6 @@
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
-import os
 import math
 import Interest_Point_using_SIFT as Interest_Point
 import Matching_using_SIFT as matching
@@ -86,15 +85,60 @@ class RansacModel(object):
         err = ( np.diag(np.dot(x1.T,np.dot(F,x2))) )**2 / denom
         return err
 
+def ransac(data,model,n,k,t,d,debug=False,return_all=False):
+
+    iterations = 0
+    bestfit = None
+    besterr = np.inf
+    best_inlier_idxs = None
+    while iterations < k:
+        maybe_idxs, test_idxs = random_partition(n,data.shape[0])
+        maybeinliers = data[maybe_idxs,:]
+        test_points = data[test_idxs]
+        maybemodel = model.fit(maybeinliers)
+        test_err = model.get_error( test_points, maybemodel)
+        also_idxs = test_idxs[test_err < t] # select indices of rows with accepted points
+        alsoinliers = data[also_idxs,:]
+        if debug:
+            print ('test_err.min()',test_err.min())
+            print ('test_err.max()',test_err.max())
+            print ('numpy.mean(test_err)',np.mean(test_err))
+            print ('iteration %d:len(alsoinliers) = %d'%(
+                iterations,len(alsoinliers)))
+        if len(alsoinliers) > d:
+            betterdata = np.concatenate( (maybeinliers, alsoinliers) )
+            bettermodel = model.fit(betterdata)
+            better_errs = model.get_error( betterdata, bettermodel)
+            thiserr = np.mean( better_errs )
+            if thiserr < besterr:
+                bestfit = bettermodel
+                besterr = thiserr
+                best_inlier_idxs = np.concatenate( (maybe_idxs, also_idxs) )
+        iterations+=1
+    if bestfit is None:
+        raise ValueError("did not meet fit acceptance criteria")
+    if return_all:
+        return bestfit, {'inliers':best_inlier_idxs}
+    else:
+        return bestfit
+
+def random_partition(n,n_data):
+    """return n random rows of data (and also the other len(data)-n rows)"""
+    all_idxs = np.arange( n_data )
+    np.random.shuffle(all_idxs)
+    idxs1 = all_idxs[:n]
+    idxs2 = all_idxs[n:]
+    return idxs1, idxs2
+
 def F_from_ransac(x1,x2,model,maxiter=5000,match_theshold=1e-6):
     '''
     The function apply threshold and the minimum number of points desired.
     The most important parameter is the maximum number of iterations, 
     exiting too early might give a worse solution, too many iterations will take more time.
     '''
-    import ransac
+    # import ransac
     data = np.vstack((x1,x2))
-    F,ransac_data = ransac.ransac(data.T,model,8,maxiter,match_theshold,20,return_all=True)
+    F,ransac_data = ransac(data.T,model,8,maxiter,match_theshold,20,return_all=True)
     return F, ransac_data['inliers']
 
 def compute_P_from_essential(E):
@@ -207,4 +251,3 @@ if __name__=='__main__':
     P1est = Reconstructing_Rest_Of_Points(Hest_c2_c1,P1est)
     
     Plot_Model(P1est)
-       
